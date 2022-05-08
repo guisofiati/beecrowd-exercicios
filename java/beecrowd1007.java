de136e43a1aa032f21853118f8fcf8ba858ef482
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-05-08 17:37:33 -0300

    Exercicio 1007

---
+++ java/beecrowd1007.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class beecrowd1007 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int DIFERENCA;
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int D = sc.nextInt();
+		
+		DIFERENCA = A * B - C * D;
+		
+		System.out.println("DIFERENCA = " + DIFERENCA);
+		
+		sc.close();
+	}
+}
